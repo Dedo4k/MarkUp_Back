1bc3b16e67fc114b4dca2a225bc431e57ca956e3
Vlad Lailo <vladislav.lailo@redpilldev.tech>
2023-03-28 02:41:55 +0300

    initial commit

---
+++ src/main/java/vlad/lailo/markup/MarkUpBackApplication.java
@@ -0,0 +1,13 @@
+package vlad.lailo.markup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkUpBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkUpBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vlad/lailo/markup/MarkUpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package vlad.lailo.markup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkUpBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
